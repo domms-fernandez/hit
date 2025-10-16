64fd44fc70d1de04e606f39d488a8901b8c5b7c4
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-16 12:38:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const KEYFRAME_AMOUNT = 20;
 let buttonPositioner = document.querySelector("div");
 let button = document.querySelector("button");
 
+let style = document.querySelector("style");
+let initialStyleHTML = style.innerHTML;
+
 button.addEventListener("click", () => {
   let shakeCSS = "\n@keyframes shake {";
-  for(let i=0; i<100; i += Math.floor(100/KEYFRAME_AMOUNT)) {
-    let randX = Math.floor(Math.random() * (100 - i));
-    shakeCSS += `${i}% {transform: translate(${randX}%, ${i}%)}`;
+  for(let i = 0; i <= 100; i += Math.floor(100/KEYFRAME_AMOUNT)) {
+    let randX = 50 - Math.floor(Math.random() * (200 - 2*i));
+    let randY = 50 - Math.floor(Math.random() * (200 - 2*i));
+    shakeCSS += `\n${i}% {transform: translate(${randX}%, ${randY}%)}`;
   }
-  shakeCSS += "}";
-  console.log(shakeCSS);
+  shakeCSS += "\n}";
+
+  style.innerHTML = initialStyleHTML + shakeCSS;
+  buttonPositioner.style = "animation: shake 1s linear 0s infinite forwards;"
 });
