6ed87402d492cb27aeaa702d272dc1894c86ef9e
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-16 12:46:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const KEYFRAME_AMOUNT = 20;
+const KEYFRAME_AMOUNT = 50;
 
 let buttonPositioner = document.querySelector("div");
 let button = document.querySelector("button");
@@ -9,8 +9,8 @@ let initialStyleHTML = style.innerHTML;
 button.addEventListener("click", () => {
   let shakeCSS = "\n@keyframes shake {";
   for(let i = 0; i <= 100; i += Math.floor(100/KEYFRAME_AMOUNT)) {
-    let randX = 50 - Math.floor(Math.random() * (200 - 2*i));
-    let randY = 50 - Math.floor(Math.random() * (200 - 2*i));
+    let randX = 150 - Math.floor(Math.random() * (200 - 2*i));
+    let randY = 150 - Math.floor(Math.random() * (200 - 2*i));
     shakeCSS += `\n${i}% {transform: translate(${randX}%, ${randY}%)}`;
   }
   shakeCSS += "\n}";
